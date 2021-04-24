da065560dfde9c80b78e669759fc24f4c530972b
JesusIasenzaniro <16jesusvicente@gmail.com>
2021-04-24 18:39:42 +0200

    Boilde plate of react-redux

---
+++ src/components/UsersComponents.js
@@ -0,0 +1,28 @@
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUsers } from '../redux/actions/users';
+import Card from './CardComponent';
+
+const Users = () => {
+    const dispatch = useDispatch();
+    const users = useSelector((state) => state.users.users);
+    const loading = useSelector((state) => state.users.loading);
+    const error = useSelector((state) => state.users.error);
+    useEffect(() => {
+        dispatch(getUsers());
+    }, []);
+
+    return (
+        <Fragment>
+            {users.loading && <p>Loading...</p>}
+            {users.length > 0 &&
+                users.map((user) => {
+                    return <Card user={user} key={user.id} />;
+                })}
+            {users.length === 0 && <p>No users available!</p>}
+            {error && !loading && <p>{error}</p>}
+        </Fragment>
+    );
+};
+
+export default Users;
